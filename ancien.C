535e8607343d5580e68793e0195600d9a1b8477a
Sviatoslav Bilokin <sbilokin@ccage030.in2p3.fr>
2014-09-20 21:09:58 +0200

    ccage

---
+++ ancien.C
@@ -0,0 +1,25 @@
+#ifndef _ancien_hh
+#define _ancien_hh
+class ancien 
+{
+public:
+	//
+	//	Constructor & Destructor
+	//
+	ancien (arguments);
+	virtual ~ancien () {};
+	//
+	//	Methods
+	//
+	Method	
+private:
+	//
+	//	Methods
+	//
+
+	//
+	//	Data
+	//
+	/* data */
+};
+#endif
